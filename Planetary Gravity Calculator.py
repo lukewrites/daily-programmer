fbff4d16d831dbbc576aa7133a20f15fac1ca165
Luke Petschauer <lpetschauer@gmail.com>
2014-06-16 09:18:08 +0800

    add readme and first project

---
+++ Planetary Gravity Calculator.py
@@ -0,0 +1,31 @@
+"""
+Input Description
+
+You will be given a number M which is the mass of an object in kilograms, on its
+own line, for example:
+
+    100
+
+Followed by a number N:
+
+    4
+
+You will then, on separate lines, be given a list of N planets. This will be
+given as its name, its radius (in metres), and its average density (in kilograms
+per cubic metre), like so:
+
+    Mercury, 2439700, 5427
+
+Output Description
+
+Print the weight (in Newtons) of the object if it were at the surface of each
+planet, like so:
+
+    Mercury: 314.623
+"""
+
+from math import pi
+
+class Planet(object):
+    """A planet class"""
+    def __init__(self, arg):
---
+++ readme.md
@@ -0,0 +1,8 @@
+This repository contains original solutions for programming challenges presented in the [/r/dailyprogrammer](http://www.reddit.com/r/dailyprogrammer/) subreddit.
+
+## Easy
+[Planatary Gravity Calculator](http://www.reddit.com/r/dailyprogrammer/comments/284mep/6142014_challenge_166b_easy_planetary_gravity/)
+
+## Intermediate
+
+## Hard
