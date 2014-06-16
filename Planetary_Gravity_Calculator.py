ebeb0381163d58586d3a3356c15aae97e759e69a
Luke Petschauer <lpetschauer@gmail.com>
2014-06-16 11:33:26 +0800

    working tests
    update tests to have correct radius & density of earth.
    added comments in class to indicate what to do
    not yet working correctly

--- Planetary_Gravity_Calculator.py
+++ Planetary_Gravity_Calculator.py
@@ -1,22 +1,15 @@
 """
-Input Description
-
 You will be given a number M which is the mass of an object in kilograms, on its
 own line, for example:
-
     100
 
 Followed by a number N:
-
     4
 
 You will then, on separate lines, be given a list of N planets. This will be
 given as its name, its radius (in metres), and its average density (in kilograms
 per cubic metre), like so:
-
     Mercury, 2439700, 5427
-
-Output Description
 
 Print the weight (in Newtons) of the object if it were at the surface of each
 planet, like so:
@@ -26,9 +19,21 @@ planet, like so:
 
 from math import pi
 
+gravity = 6.67e-11
+
+
 class Planet(object):
     """A planet class"""
     def __init__(self, name, radius, density):
         self.name = name
-        self.radius = radius
-        self.density = density
+        self.radius = radius  # in meters
+        self.density = density  # in kg/m3
+        self.volume = ((4 / 3) * pi * (self.radius ^ 3))
+        self.planet_mass = self.volume * self.density
+        # TODO add in automatic tagging of units.
+
+    def thing(self, mass):
+        self.mass = mass  # in kg
+        self.weight = gravity * ((self.mass * self.planet_mass) / 1)
+        print self.name + ':', self.weight, 'N'
+        # TODO add in unit for weight.
--- Planetary_Gravity_Calculator_Tests.py
+++ Planetary_Gravity_Calculator_Tests.py
@@ -1,1 +1,32 @@
 """Tests for Planetary_Gravity_Calculator.py"""
+import unittest
+from Planetary_Gravity_Calculator import Planet
+from math import pi
+
+
+class testPlanet(unittest.TestCase):
+
+    def setUp(self):
+        self.earth = Planet('Earth', 6367445, 5515)
+        self.earth.light_thing = self.earth.thing(25)
+        self.mercury = Planet('Mercury', 2439700, 5427)
+        self.mercury.light_thing = self.mercury.thing(100)
+
+
+    def test_if_i_can_create_a_planet(self):
+        self.assertEqual(self.earth.radius, 6367445)
+        self.assertEqual(self.earth.density, 5515)
+        self.assertAlmostEqual(self.earth.volume, 20003921.5757297)
+
+    def test_if_planet_calculates_planet_mass(self):
+        self.assertEqual(self.earth.planet_mass, (((4 / 3) * pi *
+                         (self.earth.radius ^ 3)) / self.earth.density))
+
+    def test_if_weight_is_calculated_correctly(self):
+        self.assertAlmostEqual(self.mercury.light_thing.weight, 314.623)
+
+    # def test_if_planet_calculates_thing_weight(self):
+    #     self.assertEqual(self.earth.light_thing, )
+
+if __name__ == '__main__':
+    unittest.main()
