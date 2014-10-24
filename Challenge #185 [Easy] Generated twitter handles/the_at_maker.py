cf6c90667b6c3e0882dea62d3a0d349e0a373e4b
Luke Petschauer <luke@lukewrites.com>
2014-10-24 16:15:12 +0800

    refactor for "bonus" question

--- Challenge #185 [Easy] Generated twitter handles/the_at_maker.py
+++ Challenge #185 [Easy] Generated twitter handles/the_at_maker.py
@@ -1,15 +1,17 @@
 def the_at_maker():
+    at_words = []
     with open("WordList.txt", 'r', errors="ignore") as read_words:
-        at_words = []
         for word in read_words:
-            if word[:3] == 'att':
-                at_words.append(word.replace('at', '@'))
-            elif word[:2] == 'at':
-                at_words.append(word.replace('att', '@'))
-        at_words.sort(key=len)
-        print("The short ones:")
-        for i in range(10):
-            print(at_words[i])
-        print('The long ones:')
-        for i in range(1,10):
-            print(at_words[-i])
+            if "'" not in word:
+                if word[:3] == 'att':
+                    at_words.append(word.replace('att', '@', 1).replace('at', '@'))
+
+                elif word[:2] == 'at':
+                    at_words.append(word.replace('at', '@', 1))
+    at_words.sort(key=len)
+    print("The short ones:")
+    for i in range(10):
+        print(at_words[i])
+    print('The long ones:')
+    for i in range(1,10):
+        print(at_words[-i])
